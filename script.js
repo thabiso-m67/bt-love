6496af07e159c07545928ef282d9ed186dd0d626
thabiso-m67 <smthololo@gmail.com>
2026-05-11 11:28:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function updateDaysCounter() {
 // =========================
 function checkAnniversary() {
 
-    const today = new Date("2026-05-16");
+    const today = new Date();
     const day = today.getDate();
     const month = today.getMonth() + 1;
     // 🎂 Birthday
